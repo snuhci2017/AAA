aaa323cdb9c19ad1ab861941cfde1100aab1254b
ytaek.kim <ytaek.kim@hcil.snu.ac.kr>
2017-06-05 19:49:47 +0900

    FEATURE: person table add/remove

--- webapp/js/table.js
+++ webapp/js/table.js
@@ -1,30 +1,60 @@
-function addPerson(id) {
+function addPerson(idList) {
+    var columns = ["이름", "정당", "당선#", "의안발의#", "가결#", "부결#"];
     var personTable = d3.select("#person");
-    console.log(personTable);
+
+    // table header
+    personTable.append("thead")
+        .append("tr")
+        .selectAll("th")
+        .data(columns)
+        .enter()
+        .append("th")
+        .text(function (d) {return d;});
+    
+    personTable.selectAll("thead").select("tr").append("th").text("DEL");
+
+    // table body
     d3.tsv("data/assembly_test.tsv", function(error, data) {
-        // data.forEach(function insertData(tuple) {
-        var tr = personTable.select("tbody")
+        // FILTERING
+        data = data.filter(function (d) {
+            return idList.indexOf(Number(d['id'])) >= 0;
+        });
+
+        var rows = personTable.append("tbody")
+            .selectAll("tr")
             .data(data)
             .enter()
             .append("tr")
-console.log(Object.keys(data[0]));
-        tr.append("td").html(function td(d) {return d;});
-        tr.append("td").html("aa");
-        tr.append("td").html("aa");
-        tr.append("td").html("aa");
-        tr.append("td").html("aa");
+                .attr("id", function (d) { return "person" + d['id'];})
 
-        // console.log(tuple    );
-        // });
+   		var cells = rows.selectAll('td')
+            .data(function (row) {
+                return columns.map(function (column) {
+                    return {column: column, value: row[column]};
+                });
+            })
+            .enter()
+            .append('td')
+                .text(function (d) { return d.value; });
+        
+        rows.append("td")
+            .append("button")
+            .text("X")
+            .on("click", function(d) {
+                d3.event.stopPropagation();
+                removePerson(d['id']);
+            });
+        
+        rows.on("click", function(d) { selectPerson(d['id']); });
     });
 }
 
 function removePerson(id) {
-
+    d3.select("#person" + id).remove();
 }
 
 function selectPerson(id) {
-
+    alert(id);
 }
 
 function sortPersonList(contidions) {
@@ -33,4 +63,4 @@ function sortPersonList(contidions) {
 
 
 
-addPerson();
+addPerson([1,2,3]);
