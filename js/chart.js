9f9e6df91627a5c716a292022b26fb04d268d4d6
ytaek.kim <ytaek.kim@hcil.snu.ac.kr>
2017-06-12 01:41:29 +0900

    empty id list exception case patched

--- js/chart.js
+++ js/chart.js
@@ -35,8 +35,7 @@ function addPersonToChart(id) {
     }
     drawedIdList.push(id + "");
     drawSelectedPersonList(drawedIdList);
-
-    if (drawedIdList.lenght != 0) drawChart(drawedIdList);
+    drawChart(drawedIdList);
 }
 
 function removePersonFromChart(id) {
@@ -47,7 +46,7 @@ function removePersonFromChart(id) {
     
     drawedIdList.pop(drawedIdList.indexOf(id+""));
     drawSelectedPersonList(drawedIdList);
-    if (drawedIdList.lenght != 0) drawChart(drawedIdList);
+    drawChart(drawedIdList);
 }
 
 
@@ -67,6 +66,8 @@ var yAxisRight = d3.svg.axis().scale(yRight).orient("right");
 
 function drawChart(idList) {
     d3.select("svg").remove();
+    
+    if (idList.length === 0) return;
     var chartSvg = d3.select("body").append("svg")
         .attr("width", chartSvgWidth)
         .attr("height", chartSvgHeight)
@@ -96,10 +97,6 @@ function drawChart(idList) {
             return d['DATE'];
         }));
         
-        // x = d3.time.scale().range([0, width])
-        //     .domain([d3.max(data, function(d) { return d['DATE']; }), d3.min(data, function(d) { return d['DATE']; })]);
-
-
         //////////////////////////////////////////
         // draw stackedBarChart
         var barChartData = data.reduce(function (prev, elem) {
@@ -178,19 +175,6 @@ function drawChart(idList) {
             }
         });
 
-        // idList.map(function (id) {
-        //     data.filter(function (d) { return +d['ID'] === +id; })
-        //         .reduce(function (prevVal, curVal, curIndex, array) {
-        //             var val = +prevVal + +curVal['총합계'];
-        //             cumulativeDataList[idList.indexOf(id)].push(val);
-        //             return val;
-        //         }, 0);
-        // });
-//         console.log(d3.max(cumulativeDataList, function(d) {
-//     console.log(d);
-//             return d[d.length-1].y;
-//         }));
-// return;
         yRight.domain([0, d3.max(cumulativeDataList, function (d) {
             return +d[d.length-1].y;
         })]).nice();
@@ -234,137 +218,3 @@ setTimeout(function() {
 setTimeout(function() {
     addPersonToChart(3);
 }, 2000);
-// setTimeout(function() {
-//     removePersonFromChart(2);
-// }, 3000);
-
-
-
-
-// function drawChart_bak(id) {
-//     var svg = d3.select("svg"),
-//         width = chartSvgWidth - margin.left - margin.right,
-//         height = chartSvgHeight - margin.top - margin.bottom;
-
-//     var parseTime = d3.timeParse("%y-%m");
-
-//     // var xBar = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-//     var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-//     var yLeft = d3.scale.linear().range([height, 0]);
-//     var yRight = d3.scale.linear().range([height, 0]);
-
-//     var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.time.format("%y-%m"));
-//     var yAxisLeft = d3.svg.axis().scale(yLeft).orient("left");
-//     var yAxisRight = d3.svg.axis().scale(yRight).orient("right");
-
-//     var color = d3.scale.category10();
-
-//     var svg = d3.select("body").append("svg")
-//         .attr("width", width + margin.left + margin.right)
-//         .attr("height", height + margin.top + margin.bottom)
-//         .attr("id", "billDetailChart")
-//         .append("g")
-//         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-//     d3.tsv("data/bill_detail_test.tsv", function (d) {
-//         d['DATE'] = parseTime(d['DATE']); return d;
-//     }, function (error, data) {
-//         if (error) throw error;
-        
-//         data = data.filter(function (d) {
-//             return +d['ID'] === +id;
-//         });
-
-//         var cumulativeData = [];
-//         data.reduce(function (prevVal, curVal, curIndex, array) {
-//             var val = +prevVal + +curVal['총합계'];
-//             //cumulativeData[data[curIndex]['DATE']] = val;
-//             cumulativeData.push(val);
-//             return val;
-//         }, 0);
-//         console.log(cumulativeData);
-//         // xBar.domain(data.map(function(d) {
-//         //     return d['DATE'];
-//         // }));
-//         x.domain(data.map(function (d) {
-//             return d['DATE'];
-//         }));
-//         yLeft.domain([0, d3.max(data, function (d) {
-//             return +d['총합계']
-//         })]).nice();
-//         yRight.domain([0, d3.max(cumulativeData, function (d) {
-//             return +d;
-//         })]).nice();
-//         var xAxisPos = svg.append("g")
-//             .attr("class", "x axis")
-//             .attr("transform", "translate(0," + height + ")")
-//             .call(xAxis);
-//         xAxisPos.selectAll("text")
-//             .style("text-anchor", "end")
-//             .attr("dx", "-.15em")
-//             .attr("dy", ".7em")
-//             .attr("transform", function (d) {
-//                 return "rotate(-40)"
-//             });
-
-//         xAxisPos.append("text")
-//             .attr("class", "label")
-//             .style("text-anchor", "end")
-//             .text("시간(월)")
-//             .attr("x", 10)
-//             .attr("y", 16);
-
-//         svg.append("g")
-//             .attr("class", "y axis axisLeft")
-//             .call(yAxisLeft)
-//             .append("text")
-//             .attr("transform", "rotate(-90)")
-//             .attr("y", 6)
-//             .attr("dy", ".71em")
-//             .style("text-anchor", "end")
-//             .text("의안 발의 수(건)");
-
-//         svg.selectAll("bar")
-//             .data(data)
-//             .enter().append("rect")
-//             .style("fill", "steelblue")
-//             .attr("x", function (d) {
-//                 return x(d['DATE']);
-//             })
-//             .attr("width", x.rangeBand())
-//             .attr("y", function (d) {
-//                 return yLeft(d['총합계']);
-//             })
-//             .attr("height", function (d) {
-//                 return height - yLeft(d['총합계']);
-//             });
-
-//         svg.append("g")
-//             .attr("class", "y axis axisRight")
-//             .style("text-anchor", "start")
-//             .attr("transform", "translate(" + (width) + ",0)")
-//             .call(yAxisRight)
-//             .append("text")
-//             .attr("y", -16)
-//             .attr("dy", ".71em")
-//             .style("text-anchor", "end")
-//             .attr("transform", "rotate(-90)")
-//             .text("의안 발의 수 누적(건)");
-
-//         svg.append("g")
-//             .append("path")
-//             .datum(cumulativeData)
-//             .attr("fill", "orange")
-//             .attr("stroke", "steelblue")
-//             .attr("stroke-linejoin", "round")
-//             .attr("stroke-linecap", "round")
-//             .attr("stroke-width", 1.5)
-//             .attr("d",
-//                 d3.svg.line().x(function(d, i) {
-//                     return x(data[i]['DATE']) + x.rangeBand() / 2;
-//                 }).y(function(d) {
-//                     return yRight(+d);
-//                 }));
-//         drawedIdList.push(id);
-//     });
-// }
