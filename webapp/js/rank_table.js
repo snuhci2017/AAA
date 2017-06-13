c651c6f5d0eeff1341f79cbc1cf669f95d6ed677
ytaek.kim <ytaek.kim@hcil.snu.ac.kr>
2017-06-13 16:48:48 +0900

    rankchart width control/balance

--- webapp/js/rank_table.js
+++ webapp/js/rank_table.js
@@ -7,7 +7,7 @@ var rankTableHeadTr = rankTable.append("thead").append("tr");
 var rankTableBody = rankTable.append("tbody");
 var color = d3.scale.category10();
 var masterData;
-var rankTableColumnWidth = 100;
+var rankTableColumnWidth = 600;
 var rankTableRowHeight = 15;
 
 // LOAD MASTER DATA
@@ -34,6 +34,11 @@ function init() {
 
 // LOAD BILLBYPERSON DATA
 function sortList(priorityList) {
+    // Normalizing
+    var priSum = priorityList.reduce(function(prev, cur) { return prev + cur; }, 0);
+    priorityList = priorityList.reduce(function(prev, cur) { prev.push(cur / priSum); return prev; }, []);
+
+    console.log(priorityList);
     var tableColumns = [];
     var priorityListNoZero = priorityList.reduce(function(prev, cur, i) {
         if (cur !== 0) {
@@ -66,11 +71,11 @@ function sortList(priorityList) {
     });
     console.log(rankData);
 
-    drawRankTable(tableColumns, rankData);
-    //updateGeomap(rankData);
+    drawRankTable(priorityList, tableColumns, rankData);
+    // updateGeomap(rankData);
 }
 
-function drawRankTable(tableColumns, rankData) {
+function drawRankTable(priorityList, tableColumns, rankData) {
     // CLEAR
     rankTableHeadTr
         .selectAll("th")
@@ -83,20 +88,45 @@ function drawRankTable(tableColumns, rankData) {
         .exit()
         .remove();
 
-    // rankData.value = "";
-    var thead = rankTableHeadTr
-        .selectAll("th")
-        .data(["rank", "name"].concat(tableColumns));
-
-    thead.enter()
+    priorityList = priorityList.reduce(function(prev, cur) { if (cur !== 0) prev.push(cur); return prev; }, []);
+    var pListStackForHeader = priorityList.reduce(function(prev, cur, i) {
+        if (cur === 0) return prev;
+        var cum = 0;
+        if (i > 0) cum = prev[i - 1].x0 + prev[i - 1].x;
+        prev.push({ x: cur, x0: cum });
+        return prev;
+    }, []);
+    var x = d3.scale.linear().rangeRound([0, rankTableColumnWidth]);
+    rankTableHeadTr
         .append("th")
-        .text(function(d) { return d; })
-        .style("width", function(d, i) {
-            if (i > 1) return rankTableColumnWidth + "px";
-        })
-        .style("background-color", function(d, i) {
-            if (i > 1) return color(i - 2);
-        });
+        .style("width", "20")
+        .text("rank");
+    rankTableHeadTr
+        .append("th")
+        .style("width", "50")
+        .text("name");
+    rankTableHeadTr
+        .append("th")
+        .style("width", rankTableColumnWidth)
+        .append("div")
+        .append("svg")
+        .attr("width", rankTableColumnWidth)
+        .attr("height", rankTableRowHeight)
+        .append("g")
+        .selectAll("rect")
+        .data(pListStackForHeader)
+        .enter()
+        .append("rect")
+        .attr("x", function(d) { return x(d.x0); })
+        .attr("width", function(d) { return x(d.x); })
+        .attr("y", 0)
+        .attr("height", rankTableRowHeight - 1)
+        .style("opacity", "0.0")
+        .transition()
+        .delay(300)
+        .duration(300)
+        .style("opacity", "1.0")
+        .style("fill", function(d, i) { return color(i); });
 
     var rows = rankTableBody
         .selectAll("tr")
@@ -124,8 +154,6 @@ function drawRankTable(tableColumns, rankData) {
 
 
     // STACKED BAR CHART
-    var maxWidth = tableColumns.length * rankTableColumnWidth;
-    var x = d3.scale.linear().rangeRound([0, maxWidth]).domain([0, tableColumns.length]);
     var stack = d3.layout.stack()(
         Object.entries(rankData).reduce(function(prev, elem) {
             [key, values] = elem;
@@ -139,7 +167,7 @@ function drawRankTable(tableColumns, rankData) {
         .attr("id", "bars")
         .append("div")
         .append("svg")
-        .attr("width", maxWidth)
+        .attr("width", rankTableColumnWidth)
         .attr("height", rankTableRowHeight)
         .append("g");
 
@@ -148,8 +176,8 @@ function drawRankTable(tableColumns, rankData) {
         .data(function(d) {
             return tableColumns.reduce(function(prev, cur, i) {
                 var cum = 0;
-                if (i > 0) cum = prev[i - 1].x;
-                prev.push({ x: cum + d[cur], x0: cum });
+                if (i > 0) cum = prev[i - 1].x0 + prev[i - 1].x;
+                prev.push({ x: d[cur], x0: cum });
                 return prev;
             }, []);
         });
@@ -160,6 +188,11 @@ function drawRankTable(tableColumns, rankData) {
         .attr("width", function(d) { return x(d.x); })
         .attr("y", 0)
         .attr("height", rankTableRowHeight - 1)
+        .style("opacity", "0.0")
+        .transition()
+        .delay(300)
+        .duration(300)
+        .style("opacity", "1.0")
         .style("fill", function(d, i) { return color(i); });
 
     cells.exit()
@@ -169,12 +202,12 @@ function drawRankTable(tableColumns, rankData) {
         .style('opacity', 0.0)
         .remove();
 
-    thead.exit()
-        .transition()
-        .delay(200)
-        .duration(500)
-        .style('opacity', 0.0)
-        .remove();
+    // thead.exit()
+    //     .transition()
+    //     .delay(200)
+    //     .duration(500)
+    //     .style('opacity', 0.0)
+    //     .remove();
 
     rows.exit()
         .transition()
@@ -190,121 +223,10 @@ function selectPerson(id) {
     alert(id);
 }
 
-setTimeout(function() {
-    sortList([1, 0, 0, 0, 0, 1]);
-}, 2000);
-
-setTimeout(function() {
-    sortList([1, 1, 1, 1, 1, 0]);
-}, 4000);
-
-// rankTable.append("thead")
-//     .append("tr")
-//     .selectAll("th")
-//     .data(columns)
-//     .enter()
-//     .append("th")
-//     .text(function(d) { return d; });
-
-
-
-
-
-
-// var columns = ["이름", "정당", "당선#", "의안발의#", "가결#", "부결#"];
-// var personTable = d3.select("#person");
-
-// // table header
-// personTable.append("thead")
-//     .append("tr")
-//     .selectAll("th")
-//     .data(columns)
-//     .enter()
-//     .append("th")
-//     .text(function(d) { return d; });
-
-// personTable.selectAll("thead").select("tr").append("th").text("DEL");
-// personTable.append("tbody");
-
-// function addPerson(idList) {
-
-//     // table body
-//     d3.tsv("data/assembly_test.tsv", function(error, data) {
-//         // FILTERING
-//         data = data.filter(function(d) {
-//             return idList.indexOf(Number(d['id'])) >= 0;
-//         });
-
-//         var rows = personTable.select("tbody")
-//             .selectAll("tr")
-//             .data(data);
-
-//         rows.enter()
-//             .append("tr")
-//             .attr("id", function(d) { return "person" + d['id']; });
-
-//         var cells = rows.selectAll('td')
-//             .data(function(row) {
-//                 return columns.map(function(column) {
-//                     return { column: column, value: row[column] };
-//                 });
-//             });
-
-//         cells.enter()
-//             .append('td')
-//             .style("opacity", "0.0")
-//             .transition()
-//             .delay(300)
-//             .duration(300)
-//             .style("opacity", "1.0")
-//             .text(function(d) { return d.value; });
-
-//         cells.exit()
-//             .transition()
-//             .delay(200)
-//             .duration(500)
-//             .style('opacity', 0.0)
-//             .remove();
-
-//         rows.append("td")
-//             .append("button")
-//             .text("X")
-//             .on("click", function(d) {
-//                 d3.event.stopPropagation();
-//                 removePerson(d['id']);
-//             })
-//             .style("opacity", "0.0")
-//             .transition()
-//             .delay(300)
-//             .duration(300)
-//             .style("opacity", "1.0");
-
-//         rows.on("click", function(d) { selectPerson(d['id']); });
-
-//         rows.exit()
-//             .transition()
-//             .delay(200)
-//             .duration(500)
-//             .style('opacity', 0.0)
-//             .remove();
-//     });
-// }
-
-// function removePerson(id) {
-//     d3.select("#person" + id).remove();
-// }
-
-// function selectPerson(id) {
-//     alert(id);
-// }
-
-// function sortPersonList(conditions) {
-
-// }
-
-
-
-// addPerson([1, 2, 3, 4, 5, 6, 7, 8, 9]);
 // setTimeout(function() {
-//     addPerson([1, 2, 3, 4, 5, 6]);
+//     sortList([1, 0, 0, 0, 0, 1]);
 // }, 2000);
+
+// setTimeout(function() {
+//     sortList([1, 1, 1, 1, 1, 0]);
+// }, 4000);
